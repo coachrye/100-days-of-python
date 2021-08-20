d0dd23123182b22ee9c9df6dac76cd1bcdeab28f
Coach Rye <coachrye@coachrye.lan>
2021-08-21 03:06:18 +0800

    Day 59 - Step 6 Completed

--- day-59-startbootstrap-clean-blog-gh-pages/main.py
+++ day-59-startbootstrap-clean-blog-gh-pages/main.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template
-# import random
-# import datetime
-# import requests
+import requests
 
+# blog_url = "https://api.npoint.io/daf8714af94a00a38c68"
+# response = requests.get(blog_url)
+# all_posts = response.json()
+
+all_posts = requests.get("https://api.npoint.io/daf8714af94a00a38c68").json()
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+
+    return render_template("index.html", posts=all_posts)
+
 
 @app.route('/about')
 def about():
