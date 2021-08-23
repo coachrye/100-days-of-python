9d7532728ef7838623aba80ae3ffa21c968c1359
Coach Rye <coachrye@coachrye.lan>
2021-08-24 03:48:10 +0800

    Day 63 - Before SQLite

--- day-63-library/main.py
+++ day-63-library/main.py
@@ -7,12 +7,21 @@ all_books = []
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        data = request.form
+        new_record = {
+            'title': data["book_name"],
+            'author': data["book_author"],
+            'rating': data["rating"]
+        }
+        all_books.append(new_record)
+        return redirect(url_for('home'))
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
