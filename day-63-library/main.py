0de68b2d7f1328fb4e25968c9636b88cc81c48d0
Ryan Salvanera <rye@coachrye.com>
2021-08-25 06:26:21 +0800

    Day 63 - Completed. Man installing SQLAlchemy was frustrating

--- day-63-library/main.py
+++ day-63-library/main.py
@@ -1,12 +1,48 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
+# import sqlite3
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# https://sqlitebrowser.org/dl/
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collection-two.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+db.create_all()
+
+#CREATE RECORD
+# new_book = Book(title="Harry 11 Potter", author="J. K. Rowling", rating=9.2)
+# db.session.add(new_book)
+# db.session.commit()
+
 
 all_books = []
 
 
 @app.route('/')
 def home():
+    all_books = db.session.query(Book).all()
     return render_template("index.html", books=all_books)
 
 
@@ -14,12 +50,15 @@ def home():
 def add():
     if request.method == "POST":
         data = request.form
-        new_record = {
-            'title': data["book_name"],
-            'author': data["book_author"],
-            'rating': data["rating"]
-        }
-        all_books.append(new_record)
+        # new_record = {
+        #     'title': data["book_name"],
+        #     'author': data["book_author"],
+        #     'rating': data["rating"]
+        # }
+        # all_books.append(new_record)
+        new_book = Book(title=data["book_name"], author=data["book_author"], rating=data["rating"])
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template("add.html")
 
